9db5d616776c08b94f0a161710f1d5142b9ef855
Ben Pantazon <pantazon-b@live.com>
2019-03-05 15:53:06 -0600

    Created EmployeeController. Currently adds all customers from the database to a list, which will be used for the employee to view some customer info such as their address, zip code, the date of their extra pickup, and if their pickups were confirmed

---
+++ TrashCollector/TrashCollector/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrashCollector.Models;
+
+namespace TrashCollector.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        IList<Customer> customerList = new List<Customer>() { };
+        public EmployeeController()
+        {
+            db = new ApplicationDbContext();
+        }
+        ApplicationDbContext db;
+        // GET: Employee
+        //get customers from the database for the employee to view
+        public ActionResult Index()
+        {
+            try
+            {
+                db.Customers.ToList();
+            }
+            catch
+            {
+                return View();
+            }
+            return View(customerList);
+        }
+    }
+}
